9704a440b22338b218f6ae054ec8d0cbe4141609
fernando1697 <40699917+fernando1697@users.noreply.github.com>
2018-07-02 10:19:46 -0300

    Create Ifelse.py

---
+++ Examples/Ifelse.py
@@ -0,0 +1,10 @@
+idade = 28
+
+if(idade ==10): print("TEN")
+else:
+    if(idade ==12): print("TWELVE")
+    else:
+        if(idade ==15): print("FIFTEEN")
+        else:
+            if(idade >20): print("MORE THAN TWENTY")
+
