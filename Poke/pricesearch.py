a3379e5d29b3f3677aa8311c1eab97632e8afba7
fernando1697 <40699917+fernando1697@users.noreply.github.com>
2018-07-05 11:24:00 -0300

    Update pricesearch.py

--- Poke/pricesearch.py
+++ Poke/pricesearch.py
@@ -1,20 +1,22 @@
 import requests as req
 #Input nome da carta
-
-
-
-card = input("Nome da carta : ")
+print("Quantas buscas?")
+qtd = int(input()) 
+count =0
+while(count!=qtd):
+    card = input("Nome da carta : ")
+    count = count +1
 #Requisição da busca
-requestLink = "http://pokeprices.doeiqts.com/api/findcards?cardname={}".format(card.lower())
+    requestLink = "http://pokeprices.doeiqts.com/api/findcards?cardname={}".format(card.lower())
 
 #print(requestLink)
 #pega informações depois do filtro card
-cardinfo = req.get(requestLink).json()
+    cardinfo = req.get(requestLink).json()
 
     
-for card in cardinfo['cards']:
+    for card in cardinfo['cards']:
     #print(card['name'])
         print("{} {} {} = {}$".format(card['rarity'],card['set'],card['name'],card['price'])) 
-            
-input("Pressione qualquer tecla para sair...")
+             
+input("Suas {} buscas foram finalizadas,pressione qualquer tecla para sair...".format(count))
 
