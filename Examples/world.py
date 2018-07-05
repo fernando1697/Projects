928a0a8b0ce343194c649ddb06942d47f2fbbfb7
fernando1697 <40699917+fernando1697@users.noreply.github.com>
2018-07-05 08:38:43 -0300

    Create world.py

---
+++ Examples/world.py
@@ -0,0 +1,15 @@
+import random
+import os
+#qtd = (input(""))
+count = 0
+while(count==0):
+    print("Que time paulista não tem mundial?")
+    teste = input().lower()
+   # if(teste==random.randint(1, 10)):
+    if(teste == "palmeiras"):
+        print("AHHHHHHHHHH EH VERDADE CHORA PORCADAAA")
+        os._exit(0)
+    else:
+            print("Lembre-se,nao tem mundial!")
+    #count = count +1
+print("Suas tentativas nao deram certo amigao,uma pena nao é?")
